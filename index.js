665c4a746d81e7884399aea2cd67ef4c5f0e5b01
Abdulaziz-AlQattaN <66375873+Abdulaziz-AlQattaN@users.noreply.github.com>
2020-11-01 22:37:45 +0300

    Done!

---
+++ index.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+
+ const express = require("express");
+ const mongoose = require("mongoose");
+ const setUpRoutes = require("./models/routes");
+ const cors = require("cors");
+ const cookieParser = require("cookie-parser")
+ 
+ const start = async () => {
+
+      try {
+        await mongoose.connect(process.env.DB_URL,{
+        useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+      })
+      console.log("Connected to mongoDB")
+
+        const app = express();
+        
+
+      app.use(cors())
+      app.use(express.json())
+      app.use(cookieParser())
+
+        setUpRoutes(app)
+        console.log("app routes is set up lets listen to the port ")
+
+
+      app.listen(4000);
+      } catch (error) {
+        console.error(error)
+      }
+
+      }
+
+    start()
---
+++ models/user.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require("mongoose");
+const shortId = require("shortid");
+const hashPassword = require("../helper");
+const userSchema = new Schema({
+  username: String,
+  email: { type: String, unique: true },
+  password: String,
+  salt: String,
+  Courses: [
+    { title: String, completed: Boolean },
+    { title: String, completed: Boolean },
+    { title: String, completed: Boolean },
+    { title: String, completed: Boolean },
+    { title: String, completed: Boolean },
+    { title: String, completed: Boolean },
+    { title: String, completed: Boolean },
+    { title: String, completed: Boolean },
+    { title: String, completed: Boolean },
+  ],
+});
+userSchema.pre("save", async function (next) {
+  this.Courses = [
+    { "title": "lesson1-1", "completed": false },
+    { "title": "lesson1-2", "completed": false },
+    { "title": "lesson1-3", "completed": false },
+    { "title": "lesson2-1", "completed": false },
+    { "title": "lesson2-2", "completed": false },
+    { "title": "lesson2-3", "completed": false },
+    { "title": "lesson3-1", "completed": false },
+    { "title": "lesson3-2", "completed": false },
+    { "title": "lesson3-3", "completed": false }
+  ];
+  const salt = "secret";
+  this.salt = shortId.generate();
+  this.password = await hashPassword(this.password, this.salt);
+  console.log(this.password);
+  next();
+});
+
+const userModel = new model("users", userSchema);
+module.exports = userModel;
