7b371848881b877f13fcbd738f1e210f61ae1b49
Abdulaziz-AlQattaN <66375873+Abdulaziz-AlQattaN@users.noreply.github.com>
2020-11-04 15:24:30 +0300

    donee

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ const userSchema = new Schema({
   username: String,
   email: { type: String, unique: true },
   password: String,
-  number: Number,
+  number: String,
   city: String,
   salt: String,
   Courses: [
