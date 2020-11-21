896987fc07cc61a0115a06b0f684cff7d80005f3
Abdulaziz-AlQattaN <66375873+Abdulaziz-AlQattaN@users.noreply.github.com>
2020-11-22 01:34:44 +0300

    Done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const Routes = require("./routes");
+const bodyParser = require("body-parser");
 const start = async () => {
   try {
     await mongoose.connect(process.env.DB_URL, {
@@ -16,6 +17,8 @@ const start = async () => {
 
     app.use(cors());
     app.use(express.json());
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
     app.use(Routes);
     console.log("app routes is set up lets listen to the port ");
     app.listen(process.env.PORT || 4000);
