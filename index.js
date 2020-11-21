ab0540d1f5caf15663b2f7bf88bf3bef22dc57a7
Abdulaziz-AlQattaN <66375873+Abdulaziz-AlQattaN@users.noreply.github.com>
2020-11-22 00:57:09 +0300

    Done

--- index.js
+++ index.js
@@ -1,37 +1,27 @@
-require('dotenv').config()
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const Routes = require("./routes");
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log("Connected to mongoDB");
 
- const express = require("express");
- const mongoose = require("mongoose");
- const setUpRoutes = require("./models/routes");
- const cors = require("cors");
- const cookieParser = require("cookie-parser")
- 
- const start = async () => {
+    const app = express();
 
-      try {
-        await mongoose.connect(process.env.DB_URL,{
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-      })
-      console.log("Connected to mongoDB")
+    app.use(cors());
+    app.use(express.json());
+    app.use(Routes);
+    console.log("app routes is set up lets listen to the port ");
+    app.listen(process.env.PORT || 4000);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-        const app = express();
-        
-
-      app.use(cors())
-      app.use(express.json())
-      app.use(cookieParser())
-
-        setUpRoutes(app)
-        console.log("app routes is set up lets listen to the port ")
-
-
-      app.listen(process.env.PORT || 4000);
-      } catch (error) {
-        console.error(error)
-      }
-
-      }
-
-    start()
+start();
--- models/user.js
+++ models/user.js
@@ -21,21 +21,8 @@ const userSchema = new Schema({
   ],
 });
 userSchema.pre("save", async function (next) {
-  this.Courses = [
-    { "title": "lesson1-1", "completed": false },
-    { "title": "lesson1-2", "completed": false },
-    { "title": "lesson1-3", "completed": false },
-    { "title": "lesson2-1", "completed": false },
-    { "title": "lesson2-2", "completed": false },
-    { "title": "lesson2-3", "completed": false },
-    { "title": "lesson3-1", "completed": false },
-    { "title": "lesson3-2", "completed": false },
-    { "title": "lesson3-3", "completed": false }
-  ];
-  const salt = "secret";
   this.salt = shortId.generate();
   this.password = await hashPassword(this.password, this.salt);
-  console.log(this.password);
   next();
 });
 
